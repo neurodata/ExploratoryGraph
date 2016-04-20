2e3dc7a8c29967294a3351850be5f09e24a15cd3
leo <leo@leos-MacBook-Pro.local>
2016-04-20 11:21:08 -0400

    add adjecency spectral embedding

--- Code/Functions/graphLayout.r
+++ Code/Functions/graphLayout.r
@@ -1,8 +1,9 @@
-```{r echo=TRUE,message=TRUE}
+```{r echo= FALSE,message=TRUE}
 
 require("igraph",quietly = T)
 require("networkD3",quietly = T)
 require("d3heatmap")
+require("plotly")
 # require("fields")
 
 setwd("~/git/ExploratoryGraph/Code/Functions/")
@@ -27,10 +28,43 @@ adjacency<- get.adjacency(graph,type = "both",attr = NULL,sparse = T, edges = F)
 adjacency = adjacency>0
 
 #Compute the degree
-degreeGraph<- rowSums(adjacency)
+degreeGraph<- rowSums(as.matrix(adjacency))
 orderByDegree<- order(degreeGraph,decreasing = T)
 
 ## Adjacency Matrix: ordered by degree
 # image(adjacency[orderByDegree,orderByDegree])
-d3heatmap(adjacency[orderByDegree,orderByDegree], scale = "none", colors = ("Blues"))
 
+sortedA<- adjacency[orderByDegree,orderByDegree]
+sortedD<- degreeGraph[orderByDegree]
+d3heatmap(as.matrix(sortedA), scale = "none", colors = ("Blues"))
+
+D_half<- sqrt((sortedD))
+L <-  diag(1/D_half)%*%as.matrix(sortedA) %*%diag(1/D_half)
+rownames(L) <- rownames(sortedA)
+colnames(L) <- colnames(sortedA)
+d3heatmap(L, scale = "none", colors = ("Blues"))
+
+#scree plot
+
+plot_ly(y=svdL$d)
+
+#3D embedding
+
+d<- 3
+svdL<- svd(sortedA,nu = d,nv = d)
+# plot(svdL$d)
+sigma<- svdL$d[1:d]
+X<- svdL$u %*% diag( sqrt(sigma))
+
+
+
+#let's do k-means
+
+K = 5
+kmeans_X <- kmeans(X,K)
+
+plot_ly(x= X[,1],y= X[,2],z= X[,3], type = "scatter3d",mode = "markers", color=kmeans_X$cluster)
+
+
+#degree dist
+plot_ly(x = degreeGraph,type = "histogram")
