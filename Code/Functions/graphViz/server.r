198799c794670a844902d0fd4b977efce4017c9e
leo <leo.duan.work@gmail.com>
2016-05-14 18:07:43 -0400

    updated various things

--- Code/Functions/graphViz/server.r
+++ Code/Functions/graphViz/server.r
@@ -1,7 +1,6 @@
 require("shiny")
 require("plotly")
 require("igraph", quietly = T)
-source("degreeDist.r")
 require("networkD3", quietly = T)
 source("opt_d.r")
 require("pairsD3")
@@ -9,12 +8,16 @@ require("plyr")
 require("reshape")
 
 shinyServer(function(input, output) {
-
-
+  
   # Read in the graph from graphml file
   graphFile <- reactive({
-    inFile<-input$file1
-    graph<- read.graph(inFile$datapath,
+    
+    if(input$use_demo_graph)
+      inFile<- "../../DataIngest/p.pacificus_neural.synaptic_1.graphml"
+    else
+      inFile<-input$file1$datapath
+    
+    graph<- read.graph(inFile,
      format = "graphml")
     if(!is.connected(graph)){
       graph<-decompose.graph(graph)[[1]]
@@ -23,10 +26,37 @@ shinyServer(function(input, output) {
     graph
   })
   
+  #Compute the max coreness
+  output$KinKcore <- renderUI({
+    graph <- graphFile()
+    coreness <- graph.coreness(graph, "all")
+    maxCoreness <- max(coreness)
+    sliderInput(
+      inputId = "par_k_cores",
+      label = "K:",
+      min = 1,
+      max = maxCoreness,
+      ticks = T,
+      step = 1,
+      value = 10
+    )
+  })
+  
+  #simplified graph with k core
+  graph_kcore<- reactive({
+    g<- graphFile()
+    if(input$use_k_core){
+      coreness <- graph.coreness(g, "all")
+      verticesHavingMaxCoreness <- which(coreness >= input$par_k_cores)
+      g <- induced.subgraph(graph = g, vids = verticesHavingMaxCoreness)
+    }
+    g
+  })
+  
   
   # get adjacency
   Adjacency <- reactive({
-    graph <- graphFile()
+    graph <- graph_kcore()
     a<-get.adjacency(
       graph,
       type = "both",
@@ -35,8 +65,19 @@ shinyServer(function(input, output) {
       edges = F
       )
     A = a > 0
+
     A
   })
+  
+  # get laplacian
+  subLaplacian <- reactive({
+    A <- Adjacency() 
+    denseA <- as.matrix(A) * 1
+    D<- rowSums(denseA)
+    denseA <-  diag(D) - denseA
+    denseA
+  })
+  
   
   # get normalized laplacian
   Laplacian <- reactive({
@@ -51,9 +92,15 @@ shinyServer(function(input, output) {
   # kmeans
   Cluster <- reactive({
     # L<- Laplacian()
-    A <- Adjacency() 
+    # A <- Adjacency() 
 
-    svdL<- svd(A)
+    if(input$svd_mat=="Adjacency")
+      svdL <- svd(Adjacency())
+    if(input$svd_mat=="Normalized Laplacian")
+      svdL <- svd(Laplacian())
+    if(input$svd_mat=="Laplacian")
+      svdL <- svd(subLaplacian())
+    
     u<- svdL$u[,1:input$embedding_d]
     d<- svdL$d[1:input$embedding_d]
     X<- u %*% diag(sqrt(d))
@@ -62,26 +109,12 @@ shinyServer(function(input, output) {
     # group_col= clusters$cluster
     # group_col
   })
-  #Compute the max coreness
   
-  output$KinKcore <- renderUI({
-    graph <- graphFile()
-    coreness <- graph.coreness(graph, "all")
-    maxCoreness <- max(coreness)
-    sliderInput(
-      inputId = "par_k_cores",
-      label = "K:",
-      min = 1,
-      max = maxCoreness,
-      ticks = T,
-      step = 1,
-      value = 10
-      )
-  })
+
   
   #1st viz: layout
   output$graph_layout <- renderForceNetwork({
-    graph <- graphFile()
+    graph <- graph_kcore()
     
     
 
@@ -122,7 +155,7 @@ shinyServer(function(input, output) {
   
   #2nd viz:vertex stats
   output$density_dist <- renderPlotly({
-    graph <- graphFile()
+    graph <- graph_kcore()
     A <- Adjacency()
 
     if (input$plot_degree) {
@@ -154,7 +187,7 @@ shinyServer(function(input, output) {
         aes(y = ..density..),
         alpha = 0.7,
         fill = "#333333",
-        bins = 10
+        binwidth = min_binwidth
         )
       
 
@@ -166,7 +199,7 @@ shinyServer(function(input, output) {
         aes(y = ..density..),
         alpha = 0.7,
         fill = "#333333",
-        binwidth = input$binwidth*min_binwidth/10
+        bins = input$hist_number_bins
         )
     }
     
@@ -188,31 +221,49 @@ shinyServer(function(input, output) {
   
   #3rd viz: adjacency
   output$adjacency_view <-  renderPlotly({
-    graph <- graphFile()
+    graph <- graph_kcore()
     A <- Adjacency()
     L <- Laplacian()
-    # svdL <- svd(L)
 
-
-
-    
     if(input$adjacency_mode=="Adjacency"){
       denseA <- as.matrix(A) * 1
+      caption <- "Adjacency: A"
+      
     }
     
     if(input$adjacency_mode=="Laplacian"){
+      denseA<- subLaplacian()
+      caption <- "Laplacian: D-A"
+      
+      }
+    
+    if(input$adjacency_mode=="Normalized Laplacian"){
       denseA<- L
+      caption <- "Normalized Laplacian: D^{-1/2}A D^{-1/2}"
+      
     }
     
     if(input$adjacency_mode=="Diffusion"){
       denseA <- as.matrix(A) * 1
       denseA<- diag(1/rowSums(denseA)) %*% denseA 
+      caption <- "Diffusion: D^{-1}A"
+      
     }
     
     if (input$adjacency_sortedby == "Degree")   {
       orderByDegree<- order(rowSums(denseA),decreasing = T)
       denseA <- denseA[orderByDegree, orderByDegree]
     } 
+    
+    if (input$adjacency_sortedby == "Block" &
+        input$embedding_sbm
+    )   {
+      
+      group_col = Cluster()$cluster
+      orderByBlock = order(group_col,decreasing = F)
+      denseA <- denseA[orderByBlock, orderByBlock]
+      
+    }
     
     vals <- unique(c(scales::rescale(c(denseA,1,0))))
     o <- order(vals, decreasing = FALSE)
@@ -231,7 +282,7 @@ shinyServer(function(input, output) {
     p <- ggplot(m, aes(X1, X2)) +
     geom_tile(aes(fill =  value), colour = "white") + 
     scale_fill_gradient(low = "white",  high = "red", limits=c(0,1)) +
-    theme(axis.ticks = element_blank(), axis.text.x = element_blank(),axis.text.y = element_blank())
+    theme(axis.ticks = element_blank(), axis.text.x = element_blank(),axis.text.y = element_blank()) + labs(title = caption)
 
     ggplotly(p)
 
@@ -257,15 +308,40 @@ shinyServer(function(input, output) {
 
 
   })
+  
+  
+  output$caption_adjacency <- renderText({ 
+    
+  })
 
   output$p_hat_view <- renderPlotly({
 
     if(input$embedding_sbm){
+      
+      if( input$embedding_model == 'Stochastic Block Model'){
+        cluster<- Cluster()
+        Xhat <- cluster$centers[cluster$cluster,]
+        p_hat <- Xhat %*% t(Xhat)
+      }
 
-        # L <- Laplacian()
-      cluster<- Cluster()
-      Xhat <- cluster$centers[cluster$cluster,]
-      p_hat <- Xhat %*% t(Xhat)
+      
+      if( input$embedding_model == 'Random Dot Product Model'){
+        
+        if(input$svd_mat=="Adjacency")
+          svdL <- svd(Adjacency())
+        if(input$svd_mat=="Normalized Laplacian")
+          svdL <- svd(Laplacian())
+        if(input$svd_mat=="Laplacian")
+          svdL <- svd(subLaplacian())
+        
+        u<- svdL$u[,1:input$embedding_d]
+        d<- svdL$d[1:input$embedding_d]
+        Xhat <- u %*% diag(sqrt(d))
+        
+        p_hat <- Xhat %*% t(Xhat)
+      }
+     
+      
 
 
 
@@ -275,7 +351,17 @@ shinyServer(function(input, output) {
         orderByDegree<- order(rowSums(denseA),decreasing = T)
         p_hat <- p_hat[orderByDegree, orderByDegree]
       } 
-
+      
+      
+      if (input$adjacency_sortedby == "Block" &
+          input$embedding_sbm
+          )   {
+        
+        group_col = Cluster()$cluster
+        orderByBlock = order(group_col,decreasing = F)
+        p_hat <- p_hat[orderByBlock, orderByBlock]
+        
+      }
 
       p_hat[p_hat<0]<-0
       p_hat[p_hat>1]<-1
@@ -285,11 +371,13 @@ shinyServer(function(input, output) {
       colnames(p_hat) <- c(1:nrow(p_hat))
 
       m<-melt(p_hat)
+      
+      caption <- "Estimated Probability" 
 
       p <- ggplot(m, aes(X1, X2)) +
       geom_tile(aes(fill =  value), colour = "white") + 
       scale_fill_gradient(low = "white",  high = "red", limits=c(0,1)) +
-      theme(axis.ticks = element_blank(), axis.text.x = element_blank(),axis.text.y = element_blank())
+      theme(axis.ticks = element_blank(), axis.text.x = element_blank(),axis.text.y = element_blank())+ labs(title = caption)
 
       ggplotly(p)
 
@@ -309,14 +397,45 @@ shinyServer(function(input, output) {
   
   #4th viz: spree plot
   output$spree_plot <-  renderPlotly({
-    graph <- graphFile()
-    A <- Adjacency()
-    L <- Laplacian()
-    svdL <- svd(L)
+    graph <- graph_kcore()
+   
+    if(input$svd_mat=="Adjacency")
+      svdL <- svd(Adjacency())
+    if(input$svd_mat=="Normalized Laplacian")
+      svdL <- svd(Laplacian())
+    if(input$svd_mat=="Laplacian")
+      svdL <- svd(subLaplacian())
     
     spree_ev <- svdL$d
     
-    plot_ly(y = spree_ev)
+    n<- length(spree_ev)
+    x<- c(1:n)
+    y<- spree_ev
+    z<- rep(1,n)
+    
+    
+    if (input$elbow_detect) {
+      eb1 <- opt_d(y)
+      if (eb1 < n) {
+        z[(eb1 + 1):n] <- 2
+        eb2 <- opt_d(y[(eb1 + 1):n]) + eb1
+        
+        if (eb2 < n) {
+          z[(eb2 + 1):n] <- 3
+          eb3 <- opt_d(y[(eb2 + 1):n]) + eb2
+          if (eb3 < n)
+            z[(eb3 + 1):n] <- 4
+        }
+      }
+    }
+    
+    df2 <- as.data.frame(cbind( x,y,z ))
+    
+                                    
+    p<- ggplot(df2, aes(x=x, y=y, col=z)) + geom_line()
+    
+    ggplotly(p)
+    
 
   })
   
@@ -324,7 +443,7 @@ shinyServer(function(input, output) {
   
   
   output$max_ev_range <- renderUI({
-    graph <- graphFile()
+    graph <- graph_kcore()
     A <- Adjacency()
     
     sliderInput("eigenvector_range", "View Eigenvectors (up to 9):",
@@ -333,10 +452,15 @@ shinyServer(function(input, output) {
 
   output$pD3 <-  renderPairsD3({
 
-    graph <- graphFile()
+    graph <- graph_kcore()
     A <- Adjacency()
     L <- Laplacian()
-    svdL <- svd(L)
+    if(input$svd_mat=="Adjacency")
+      svdL <- svd(Adjacency())
+    if(input$svd_mat=="Normalized Laplacian")
+      svdL <- svd(Laplacian())
+    if(input$svd_mat=="Laplacian")
+      svdL <- svd(subLaplacian())
     
     a <- input$eigenvector_range[1]
     b <- min(input$eigenvector_range[2], input$eigenvector_range[1]+8)
--- Code/Functions/graphViz/ui.r
+++ Code/Functions/graphViz/ui.r
@@ -2,57 +2,94 @@ require("shiny")
 require("plotly")
 
 # source("degreeDist.r")
-source("graphLayout.r")
+# source("graphLayout.r")
 # source("adjacencyPlot.r")
 
 
 require("networkD3", quietly = T)
 
-shinyUI(fluidPage(style="padding-top: 80px;",
-
+shinyUI(fluidPage(
+  style = "padding-top: 80px;",
+  
   absolutePanel(
-    top = 20, right = 20, width = 300,
+    top = 20,
+    right = 20,
+    width = 300,
     fixed = TRUE,
     draggable = TRUE,
     
     wellPanel(
-
       titlePanel("GraphX"),
+      
+      checkboxInput("use_demo_graph", "Demo Graph",  value = TRUE),
+      
+      
       fileInput('file1', 'Upload Graph',
-        accept=c('.graphml')),
+                accept = c('.graphml')),
+      
+      
+      
+      
       checkboxInput(
         inputId = "plot_degree",
         label = strong("Vertex Statistics"),
         value = FALSE
-        ),
+      ),
       checkboxInput(
         inputId = "plot_adjacencyy",
         label = strong("Adjacency Matrix"),
-        value = FALSE
-        ),
+        value = TRUE
+      ),
       checkboxInput(
         inputId = "plot_layout",
         label = strong("Graph Layout"),
         value = FALSE
-        ),
-
+      ),
+      
       checkboxInput(
         inputId = "plot_spree",
-        label = strong("Spree & Pairs Plot"),
+        label = strong("Spree Plot"),
         value = FALSE
-        )
       ),
-
-
+      
+      checkboxInput(
+        inputId = "plot_pairs",
+        label = strong("Pairs Plot"),
+        value = FALSE
+      )
+    ),
+    
+    
     wellPanel(
-
+      selectInput(
+        inputId = "svd_mat",
+        label = "Using the spectral decomp of",
+        choices = c(
+          "Adjacency",
+          "Normalized Laplacian",
+          "Laplacian"
+        ),
+        selected = "Normalized Laplacian"
+      ),
+      
       checkboxInput(
         inputId = "embedding_sbm",
-        label = strong("Run Stochastic Block Model?"),
+        label = strong("Embedding?"),
         value = FALSE
-        ),
+      ),
       conditionalPanel(
         "input.embedding_sbm == true",
+        
+        selectInput(
+          inputId = "embedding_model",
+          label = "Model",
+          choices = c(
+            "Random Dot Product Model",
+            "Stochastic Block Model"
+          ),
+          selected = "Stochastic Block Model"
+        ),
+        
         sliderInput(
           inputId = "embedding_d",
           label = "Latent dimension (d):",
@@ -61,7 +98,10 @@ shinyUI(fluidPage(style="padding-top: 80px;",
           ticks = T,
           step = 1,
           value = 5
-          ),
+        ),
+        
+        conditionalPanel(
+          "input.embedding_model == 'Stochastic Block Model'",
         sliderInput(
           inputId = "embedding_k",
           label = "Blocks (K):",
@@ -70,17 +110,27 @@ shinyUI(fluidPage(style="padding-top: 80px;",
           ticks = T,
           step = 1,
           value = 5
-          )
         )
+        )
+      ),
+      
+      checkboxInput(
+        inputId = "use_k_core",
+        label = strong("Use k-core?"),
+        value = FALSE
+      ),
+      conditionalPanel(
+        "input.use_k_core == true",
+       uiOutput("KinKcore")
       )
-    ),
+      
+    )
+  ),
   
   mainPanel(
-
-
     conditionalPanel(
       "input.plot_degree == true",
-
+      
       selectInput(
         inputId = "vertex_stats",
         label = "Vertex Statistics:",
@@ -89,39 +139,39 @@ shinyUI(fluidPage(style="padding-top: 80px;",
           "Betweenness Centrality",
           "Closeness Centrality",
           "Eigenvector Centrality"
-          ),
-        selected = "Degree"
         ),
-
+        selected = "Degree"
+      ),
+      
       plotlyOutput(outputId = "density_dist", height = "300px"),
-
+      
       checkboxInput(
         inputId = "show_histogram",
         label = strong("Show histogram"),
         value = FALSE
-        ),
-
-        #show this only if we bin the observations and show it in histogram
+      ),
+      
+      #show this only if we bin the observations and show it in histogram
       conditionalPanel(
         condition = "input.show_histogram == true",
         sliderInput(
-          inputId = "binwidth",
-          label = "Bin width:",
+          inputId = "hist_number_bins",
+          label = "No. of Bins:",
           min = 1,
-          max = 50,
-          ticks = T,
+          max = 20,
+          ticks = F,
           step = 1,
           value = 10
-          )
-        ),
-
+        )
+      ),
+      
       checkboxInput(
         inputId = "density",
         label = strong("Show density estimate"),
         value = FALSE
-        ),
-
-        # Display this only if the density is shown
+      ),
+      
+      # Display this only if the density is shown
       conditionalPanel(
         condition = "input.density == true",
         sliderInput(
@@ -131,65 +181,83 @@ shinyUI(fluidPage(style="padding-top: 80px;",
           max = 1,
           value = .5,
           step = 0.2
-          )
         )
-      ),
-
-
+      )
+    ),
+    
+    
     conditionalPanel(
       "input.plot_adjacencyy == true",
       selectInput(
         inputId = "adjacency_mode",
         label = "View:",
-        choices = c("Adjacency", "Laplacian", "Diffusion"),
+        choices = c("Adjacency", "Laplacian", "Normalized Laplacian", "Diffusion"),
         selected = "Adjacency"
-        ),
-
-      column(3,
-
-        plotlyOutput(outputId = "adjacency_view", height = 500, width =600)
-        ),
-
-
-      column(3, offset = 3,
-
+      ),
+      
+      textOutput("caption_adjacency"),
+      
+      column(
+        3,
+        
+        plotlyOutput(
+          outputId = "adjacency_view",
+          height = 500,
+          width = 600
+        )
+      ),
+      
+      
+      column(
+        3,
+        offset = 3,
+        
         conditionalPanel(
           "input.embedding_sbm == true",
-          plotlyOutput(outputId = "p_hat_view", height = 500,  width =600)
+          plotlyOutput(
+            outputId = "p_hat_view",
+            height = 500,
+            width = 600
           )
-        ),
-
+        )
+      ),
+      
       selectInput(
         inputId = "adjacency_sortedby",
         label = "Sorted By:",
-        choices = c("Unsorted", "Degree"),
+        choices = c("Unsorted", "Degree", "Block"),
         selected = "Unsorted"
-        )
-
-
-      ),
-
+      )
+      
+      
+    ),
+    
     
     
     conditionalPanel(
       "input.plot_layout == true",
-      forceNetworkOutput(outputId = "graph_layout", height = "500px"),
-      checkboxInput(
-        inputId = "use_k_core",
-        label = strong("Use k-core?"),
-        value = FALSE
-        ),
-      uiOutput("KinKcore")
-      ),
-
+      forceNetworkOutput(outputId = "graph_layout", height = "500px")
+    ),
+    
+    #spree plot
     conditionalPanel(
       "input.plot_spree == true",
-        # numericInput("spectral_d_to_view", "Number of eigenvalues to keep:", 5),
+      # numericInput("spectral_d_to_view", "Number of eigenvalues to keep:", 5),
       plotlyOutput(outputId = "spree_plot", height = 300),
+      checkboxInput(
+        inputId = "elbow_detect",
+        label = strong("Elbow Detection"),
+        value = FALSE
+      )
+    ),
+    
+    
+    #pairs plot
+    conditionalPanel(
+      "input.plot_pairs == true",
       uiOutput("max_ev_range"),
       uiOutput("pairsplot")
-      )
     )
   )
-)
+))
 # )
