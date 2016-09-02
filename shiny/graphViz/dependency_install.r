947605c6c95ba5300086a9f60538f905f318e7bf
laplacianduan <leo.duan.work@gmail.com>
2016-09-02 18:31:15 -0400

    script to install dependency

---
+++ shiny/graphViz/dependency_install.r
@@ -0,0 +1,9 @@
+
+install.packages("shiny")
+install.packages("plotly")
+install.packages("igraph")
+install.packages("networkD3")
+install.packages("pairsD3")
+install.packages("plyr")
+install.packages("reshape")
+install.packages("shinydashboard")
